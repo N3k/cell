683d7f5a49acbf37cc0a149af4f2686e99614f85
Cyril <ebk0000@gmail.com>
2015-11-30 20:19:35 +0100

    Cellules trop opaques en haute resolution

--- js/cell.js
+++ js/cell.js
@@ -52,7 +52,7 @@ Cell.prototype = {
       // Dessin de la cellule
       var size = param.minDistance/2*((param.generationTime*2)-this.canGenerate)/param.generationTime/2,
       light = (100-((this.life/(param.lifetime*param.generationTime*(1.5))*50)|0));
-      ctx.globalAlpha=0.9;
+      ctx.globalAlpha=0.7;
       if(size<=0) size=0.1;
       var grd = ctx.createRadialGradient(this.x + this.world.centerX, this.y + this.world.centerY, 0, this.x + this.world.centerX, this.y + this.world.centerY, size);
       
